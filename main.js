f45ed76e33fc337ba9bdf87589c59d7064721af8
Madhu Patel PSPK <pudarimadhupatel@gmail.com>
2025-03-10 11:53:30 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Scroll Reveal Settings 
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.portfolio-img', {interval: 200});￼Enter
